39612810164eebfa4628cb69f3b8bd3ee17d6bd5
Alejandro <alejandro.suau@gmail.com>
2024-05-28 11:59:50 +0200

    Improve Vec2 class

--- include/engine/utils/Vec2.h
+++ include/engine/utils/Vec2.h
@@ -2,12 +2,23 @@
 
 class Vec2 {
 public:
-    Vec2(float x, float y);
-    
-    float GetX() const;
-    float GetY() const;
+    float x, y;
 
-private:
-    float x_;
-    float y_;
+    Vec2() : x(0.f), y(0.f) {}
+    Vec2(float x_, float y_) : x(x_), y(y_) {}
+
+    Vec2 operator+(const Vec2& other) const;
+    Vec2 operator-(const Vec2& other) const;
+    Vec2 operator*(float scalar) const;
+    Vec2 operator/(float scalar) const;
+
+    Vec2& operator+=(const Vec2& other);
+    Vec2& operator-=(const Vec2& other);
+    Vec2& operator*=(float scalar);
+    Vec2& operator/=(float scalar);
+
+    float Length() const;
+    float LengthSquared() const;
+    Vec2 Normalized() const;
+    float Dot(const Vec2& other) const;
 };
