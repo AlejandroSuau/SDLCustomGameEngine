a4102d7bd9f6ad08c4e3b05a583ec0502b93d75e
Alejandro <alejandro.suau@gmail.com>
2024-07-09 17:50:13 +0200

    Engine - Countdown is loopable. Not needed to reset manually

--- include/engine/timer/CountdownTimer.h
+++ include/engine/timer/CountdownTimer.h
@@ -4,7 +4,7 @@
 
 class CountdownTimer : public Timer {
 public:
-    CountdownTimer(float seconds_to_finish, float starting_seconds = 0.f);
+    CountdownTimer(float seconds_to_finish, float starting_seconds = 0.f, bool is_looping = true);
 
     // Timer
     void Update(float dt) override;
@@ -19,4 +19,5 @@ public:
 private:
     float seconds_to_finish_;
     bool did_finish_;
+    bool is_looping_;
 };
--- src/engine/timer/CountdownTimer.cpp
+++ src/engine/timer/CountdownTimer.cpp
@@ -1,13 +1,20 @@
 #include "engine/timer/CountdownTimer.h"
 
-CountdownTimer::CountdownTimer(float seconds_to_finish, float starting_seconds) 
+#include <iostream>
+
+CountdownTimer::CountdownTimer(float seconds_to_finish, float starting_seconds, bool is_looping) 
     : Timer(starting_seconds)
     , seconds_to_finish_(seconds_to_finish)
-    , did_finish_(false) {}
+    , did_finish_(false)
+    , is_looping_(is_looping) {}
+
 
 void CountdownTimer::Update(float dt) {
     Timer::Update(dt);
     did_finish_ = (elapsed_seconds_ >= seconds_to_finish_);
+    if (did_finish_ && is_looping_) {
+        Reset();
+    }
 }
 
 bool CountdownTimer::DidFinish() const {
@@ -15,8 +22,7 @@ bool CountdownTimer::DidFinish() const {
 }
 
 void CountdownTimer::Reset() {
-    elapsed_seconds_ = 0;
-    did_finish_ = false;
+    elapsed_seconds_ = 0.f;
 }
 
 void CountdownTimer::AddSecondsToFinish(float seconds) {
@@ -25,7 +31,7 @@ void CountdownTimer::AddSecondsToFinish(float seconds) {
 
 void CountdownTimer::SubtractSecondsToFinish(float seconds) {
     seconds_to_finish_ -= seconds;
-    if (seconds_to_finish_ <= 0) {
+    if (seconds_to_finish_ <= 0.f) {
         seconds_to_finish_ = .001f;
     }
 }
